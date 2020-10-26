1392f412330abe00b5bad97d71ebdf61949d50b5
issyzac <issyzac.iz@gmail.com>
2020-10-26 14:51:31 +0300

    load image

---
+++ src/components/Project.js
@@ -0,0 +1,28 @@
+function Project(){
+    return (
+        <a class="js-editable-target editable project-cover js-project-cover-touch hold-space"
+              href="awesome-ios-ui-kit.html" data-context="pages"
+              data-identity="id:p5acdd0120aabd202770ce9f8d28a0bbc08784c034a7bc5e17e0e8">
+              <div class="cover-content-container">
+                <div class="cover-image-wrap">
+                  <div class="cover-image">
+                    <div class="cover cover-normal">
+                    <img class="cover__img js-lazy"
+                        src="https://play-lh.googleusercontent.com/uRharscO-5WJ7LZzhaqUzHZZmuVcIyvsvFMzmGIEoGGxL9P2TQxERg_Jsq-TYGdV9VJ9=w1875-h985-rw">
+                    </img>
+                    </div>
+                  </div>
+                </div>
+                <div class="details-wrap">
+                  <div class="details">
+                    <div class="details-inner">
+                      <div class="title preserve-whitespace">Awesome iOS UI Kit</div>
+                      <div class="custom2 preserve-whitespace">UI/UX, Mobile Design, UI Kit, My Product</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+    )
+}
+export default Project;
