59586bccf89939683808076c4b91e09c075621ab
issyzac <issyzac.iz@gmail.com>
2020-10-26 14:05:17 +0300

    break down view into components

---
+++ src/pages/Index.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import IndexDump from "../components/IndexDump";
+
+class Index extends Component {
+    render(){
+        return(
+            <IndexDump />
+        );
+    }
+}
+
+export default Index;
