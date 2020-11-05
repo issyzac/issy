69a59a47b6ce373557570f73aa494ee61fb23656
issyzac <issyzac.iz@gmail.com>
2020-11-06 00:03:51 +0300

    contents update

--- src/components/MainContents.js
+++ src/components/MainContents.js
@@ -21,17 +21,17 @@ function getHobbies(){
 
     let hobbies = [
         {
-            hobbyName:"The Developer",
+            hobbyName:"I Code",
             thumbnailUrl: "https://miro.medium.com/max/640/1*ob_8Ko_xuN6obuThOe3QHQ.png",
             description: "Here are some of the projects that I have been a part of"
         },        
         {
-            hobbyName:"The Photographer",
+            hobbyName:"I Shoot",
             thumbnailUrl: "https://assets.entrepreneur.com/content/3x2/2000/20191009140007-GettyImages-1053962188.jpeg",
             description: "Pationate about product photography here are some of my photography works (Amature alert)"
         },
         {
-            hobbyName:"The Barista",
+            hobbyName:"I Brew",
             thumbnailUrl: "https://sunrisenews.co/wp-content/uploads/2020/07/Best-Barista-Schools-In-The-World.jpg",
             description: "I do everything with coffee and my love for coffee has led me to explore a future as a barista, check this out"
         },
