3e80e1f8b9237d6ff8101d06d95da832cbb7d5c4
issyzac <issyzac.iz@gmail.com>
2020-11-07 12:32:20 +0300

    Changed page logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import {ReactComponent as IssyLogo} from '../res/svg/logo.svg';
+
 function PageHeader(){
     return (
         <header class="site-header">
@@ -5,16 +7,10 @@ function PageHeader(){
             <div class="logo-wrap js-editable-target editable">
               <div class="logo logo-image has-rollover ">
                 <a href="#" class="image-normal image-link">
-                  <img
-                    src="https://pro2-bar-s3-cdn-cf4.myportfolio.com/ac05157db7a7732302b941275878c68b/57117200-c9e3-41ef-8c56-1cfad47c13fa_rwc_0x0x128x128x4096.png?h=af96f62639b46477c5ef36308ff2117d"
-                    alt="Robert Licau">
-                </img>
+                <span class="ec ec-grinning"></span>
                 </a>
                 <a href="#" class="image-rollover image-link">
-                  <img
-                    src="https://pro2-bar-s3-cdn-cf6.myportfolio.com/ac05157db7a7732302b941275878c68b/7194b706-a166-496e-bef1-bdb4c16a9881_rwc_0x0x128x128x4096.png?h=670ce358c1bf73a7f1c0c7ce91b467fc"
-                    alt="Robert Licau">
-                </img>
+                <span class="ec ec-grinning"></span>
                 </a>
               </div>
             </div>
